9d661afdb0ad5262fe032278256aac14745389c9
Natan.Rocha <natan.rocha@codeandsoul.com>
2025-05-26 16:08:25 -0300

    (fix): Adjusting the aspect ratio

--- src/app/components/UI/ProjectCard.tsx
+++ src/app/components/UI/ProjectCard.tsx
@@ -20,15 +20,16 @@ export function ProjectCard({ project, onClick }: ProjectCardProps) {
       className="cursor-pointer bg-neutral-800 rounded-xl shadow-md transform transition-all duration-300 ease-in-out hover:shadow-xl hover:-translate-y-1 hover:scale-[1.015] hover:bg-neutral-700"
       aria-label={`Abrir detalhes do projeto ${project.title}`}
     >
-      <div className="flex flex-col bg-white">
-        <Image
-          src={project.image}
-          alt={`Imagem do projeto ${project.title}`}
-          width={300}
-          height={190}
-          className="rounded-t w-[300px] h-[190px] scale-[0.95]"
-          loading="lazy"
-        />
+      <div className="flex flex-col bg-white ">
+        <div className="relative w-full max-w-[300px] aspect-[300/190]">
+          <Image
+            src={project.image}
+            alt={`Imagem do projeto ${project.title}`}
+            fill
+            loading="lazy"
+            className="rounded-t object-cover scale-95"
+          />
+        </div>
       </div>
       <div className="p-4">
         <h3 className="text-base font-extrabold text-white">{project.title}</h3>
